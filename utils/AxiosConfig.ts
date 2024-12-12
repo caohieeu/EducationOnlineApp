ca83f7fb48de13942f205595ceb3a0f850036daf
caohieeu <caohieeu2003@gmail.com>
2024-12-12 23:08:09 +0700

    change url

--- utils/AxiosConfig.ts
+++ utils/AxiosConfig.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "http://10.0.2.2:80",
-  baseURL: "https://api.hightfive.click",
+  baseURL: "http://10.0.2.2:80",
+  // baseURL: "https://api.hightfive.click",
   withCredentials: true,
 })
 
